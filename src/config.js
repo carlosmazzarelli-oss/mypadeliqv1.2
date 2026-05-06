3dc0e003bfca120002cbdfe736aa832a4f88c7a7
carlosmazzarelli-oss <carlos.mazzarelli@gmail.com>
2026-05-05 22:21:54 -0700

    Update APP_URL to new Netlify URL

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const TAGLINE = "Standardized Padel Player Assessment System"
 export const VERSION = "1.0"
 
 // Replace with your scoring app's Netlify URL
-export const APP_URL = "https://padeliqappv2.netlify.app";
+export const APP_URL = "https://padeliqappv3.netlify.app";
